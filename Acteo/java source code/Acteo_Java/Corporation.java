c19b3d4119ca43cb2c5fce55111b8d4b0957b477
Katsigiannis Fotis <katsigiannis.ac@gmail.com>
2016-12-14 20:35:52 +0200

    Constructors for Corporation, Individual
    Created a folder entitled "Acteo_Java" which is going to be the package
    file.
    
    Included the .class & .java files of the constructors

---
+++ Acteo/java source code/Acteo_Java/Corporation.java
@@ -0,0 +1,76 @@
+package Acteo_Java;
+
+import java.util.Date;
+
+public class Corporation {
+
+	private String password;
+	private String email;
+	private String name;
+	private String logoLink;
+	private String description;
+	private String website;
+
+
+	//
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	//
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	//
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	//
+	public String getLogoLink() {
+		return logoLink;
+	}
+
+	public void setLogoLink(String logoLink) {
+		this.logoLink = logoLink;
+	}
+	//
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	//
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
+
+
+	public Corporation(String password, String email, String name, String logoLink, String description, String website) {
+		this.password = password;
+		this.email = email;
+		this.name = name;
+		this.logoLink = logoLink;
+		this.description = description;
+		this.website = website;
+	}
+
+} //End of class
